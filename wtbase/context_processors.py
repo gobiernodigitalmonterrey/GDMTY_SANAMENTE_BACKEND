90a778532b9bfd97f4e790c23d8eb349167f6b46
Niji <mathereall@gmail.com>
2024-05-09 09:05:43 -0600

    wip move to use environment variables instead of local.py and sa files

--- wtbase/context_processors.py
+++ wtbase/context_processors.py
@@ -1,13 +1,7 @@
-import os
 from django.conf import settings
 
 
-def recaptcha_sitekey(request):
-    # TODO: Willy, hay que considerar dos tipos de sitekey, una con challenge y otra sin eso
-    # en los settings de https://github.com/gobiernodigitalmonterrey/gdmty-django-recaptcha-enterprise/ vienen:
-    # RECAPTCHA_ENTERPRISE_SITE_KEY_VERIFY
-    # RECAPTCHA_ENTERPRISE_SITE_KEY_CHALLENGE
-
+def recaptcha(request):
     return {
         'RECAPTCHA_ENTERPRISE_SITE_KEY_VERIFY': settings.RECAPTCHA_ENTERPRISE_SITE_KEY_VERIFY,
         'RECAPTCHA_ENTERPRISE_SITE_KEY_CHALLENGE': settings.RECAPTCHA_ENTERPRISE_SITE_KEY_CHALLENGE
