581ebcce5567cb602cb39c42440c1dc71386f47b
César Benjamín <mathereall@gmail.com>
2024-05-09 15:27:01 -0600

    RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS

--- MTY_SANAMENTE_BACKEND/settings/recaptcha.py
+++ MTY_SANAMENTE_BACKEND/settings/recaptcha.py
@@ -1,5 +1,5 @@
-import os
-import ast
+import os, ast
+import json
 from google.oauth2 import service_account
 import logging
 
@@ -9,8 +9,11 @@ recaptcha_logger.info("Mostrando log de recaptcha settings")
 RECAPTCHA_ENTERPRISE_PROJECT_ID = os.getenv("RECAPTCHA_ENTERPRISE_PROJECT_ID")
 RECAPTCHA_ENTERPRISE_SITE_KEY_VERIFY = os.getenv("RECAPTCHA_ENTERPRISE_SITE_KEY_VERIFY")
 RECAPTCHA_ENTERPRISE_SITE_KEY_CHALLENGE = os.getenv("RECAPTCHA_ENTERPRISE_SITE_KEY_CHALLENGE")
-RECAPTCHA_ENTERPRISE_BYPASS_TOKEN = os.getenv("RECAPTCHA_ENTERPRISE_BYPASS_TOKEN") if os.getenv("RECAPTCHA_ENTERPRISE_BYPASS_TOKEN") else False
+RECAPTCHA_ENTERPRISE_BYPASS_TOKEN = os.getenv("RECAPTCHA_ENTERPRISE_BYPASS_TOKEN") if os.getenv(
+    "RECAPTCHA_ENTERPRISE_BYPASS_TOKEN") else False
 if os.getenv("RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS") is not None:
     recaptcha_logger.info("RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS")
     recaptcha_logger.info(os.getenv("RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS"))
-    RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS = service_account.Credentials.from_service_account_info(ast.literal_eval(os.getenv("RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS")))
+    RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS = service_account.Credentials.from_service_account_info(
+        json.loads(os.getenv("RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS"))
+    )
