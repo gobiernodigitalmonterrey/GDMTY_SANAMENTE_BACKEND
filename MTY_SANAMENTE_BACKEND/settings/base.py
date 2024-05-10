c71b1a72f1d294eed85f5a64919d07379f36f019
César Benjamín <mathereall@gmail.com>
2024-05-09 20:18:27 -0600

    RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS

--- MTY_SANAMENTE_BACKEND/settings/base.py
+++ MTY_SANAMENTE_BACKEND/settings/base.py
@@ -109,10 +109,12 @@ WSGI_APPLICATION = "MTY_SANAMENTE_BACKEND.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'spatialite://db.spatialite')
+DATABASE_URL = os.getenv('DATABASE_URL', 'spatialite:///db.spatialite')
+
+database_config = dj_database_url.parse(DATABASE_URL)
 
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL)
+    "default": database_config
 }
 
 # Password validation
