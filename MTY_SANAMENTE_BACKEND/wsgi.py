b5192ad962c3c89b01a42c66f41ed4dde6366996
Niji Saeba Media Noche <mathereall@gmail.com>
2024-05-12 21:52:45 -0600

    prueba envs

--- MTY_SANAMENTE_BACKEND/wsgi.py
+++ MTY_SANAMENTE_BACKEND/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-import ast
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MTY_SANAMENTE_BACKEND.settings.dev")
